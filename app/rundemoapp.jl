6fab2927372cd4c558ae1f23db2bff31087e7019
Christoph Finkensiep <chfin@chfin.de>
2018-11-13 14:46:56 +0100

    add ommitted parentheses

--- app/rundemoapp.jl
+++ app/rundemoapp.jl
@@ -6,19 +6,19 @@ using DigitalMusicology
 
 corpora = Dict(
     "mozart-piano-sonatas" => (
-        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "mozart-piano-sonatas"),
+        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "mozart-piano-sonatas")),
         "Mozart Piano Sonatas"
     ),
     "beethoven-piano-sonatas" => (
-        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "beethoven-piano-sonatas"),
+        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "beethoven-piano-sonatas")),
         "Beethoven Piano Sonatas"
     ),
     "beethoven-string-quartets" => (
-        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "beethoven-string-quartets"),
+        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "beethoven-string-quartets")),
         "Beethoven String Quartets"
     ),
     "chopin-mazurkas" => (
-        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "chopin-mazurkas"),
+        kerncrp(joinpath(@__DIR__, "..", "data", "corpora", "chopin-mazurkas")),
         "Chopin Mazurkas"
     )
 )
